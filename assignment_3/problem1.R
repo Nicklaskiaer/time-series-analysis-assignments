efc06410652d44e1bfd8612d98c93d25d2a710ea
Asmus Dalsgaard <weloveaus@gmail.com>
2026-04-06 11:41:16 +0200

    Create problem1.R

---
+++ assignment_3/problem1.R
@@ -0,0 +1,81 @@
+# Set seed for reproducibility
+set.seed(123)
+
+# Number of observations
+n <- 200
+
+# Number of realizations
+n_sim <- 5
+
+# Simulate 5 realizations of the AR(2) process
+# Model in assignment: X_t + phi1*X_{t-1} + phi2*X_{t-2} = e_t
+# In arima.sim(), AR signs are flipped, so we use ar = c(-phi1, -phi2)
+sim_data <- replicate(
+  n_sim,
+  arima.sim(n = n, model = list(ar = c(0.6, -0.5)))
+)
+
+# Plot all 5 realizations in one figure
+matplot(
+  sim_data,
+  type = "l",        # line plot
+  lty = 1,           # solid lines
+  xlab = "Time",
+  ylab = expression(X[t]),
+  main = "5 realizations of the AR(2) process"
+)
+
+# Add legend
+legend(
+  "topright",
+  legend = paste("Realization", 1:5),
+  lty = 1,
+  col = 1:5,
+  bty = "n"
+)
+
+
+##############################################################################################################
+# problem 1.2 
+
+# Number of lags
+lag_max <- 30
+
+# --- Theoretical ACF ---
+# Compute theoretical ACF using ARMAacf
+acf_theoretical <- ARMAacf(ar = c(0.6, -0.5), lag.max = lag_max)
+
+# --- Empirical ACF ---
+# Compute ACF for each of the 5 simulations
+acf_empirical <- apply(sim_data, 2, function(x) {
+  acf(x, plot = FALSE, lag.max = lag_max)$acf
+})
+
+# --- Plot ---
+# Plot first empirical ACF
+plot(
+  0:lag_max,
+  acf_empirical[,1],
+  type = "l",
+  ylim = range(acf_empirical, acf_theoretical),
+  xlab = "Lag",
+  ylab = "ACF",
+  main = "Empirical vs Theoretical ACF"
+)
+
+# Add remaining empirical ACFs
+for(i in 2:ncol(acf_empirical)){
+  lines(0:lag_max, acf_empirical[,i], col = i)
+}
+
+# Add theoretical ACF (thick black line)
+lines(0:lag_max, acf_theoretical, lwd = 3, col = "black")
+
+# Legend
+legend(
+  "topright",
+  legend = c("Empirical (5 sims)", "Theoretical"),
+  col = c("gray", "black"),
+  lwd = c(1,3),
+  bty = "n"
+)
